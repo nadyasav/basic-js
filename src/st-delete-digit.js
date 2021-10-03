dcdfc92e1e74c5605b9fd1951df5d507680cde59
NSavkova <47262112+NSavkova@users.noreply.github.com>
2021-10-03 18:52:15 +0300

    complete task st-delete-digit

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+  //throw new NotImplementedError('Not implemented');
+  let arrayAllDigits = Array.from(n.toString());
+  let newArray = '';
+  let minDigit = Math.min.apply(null, arrayAllDigits);
+
+  for(let i = 0; i < arrayAllDigits.length; i++){
+    if(arrayAllDigits[i] != minDigit){
+        newArray = newArray + arrayAllDigits[i];
+    }else{
+      minDigit = null;
+    }
+  }
+
+  const result = Number(newArray);
+  return result;
 }
